75e1ff3b4c0265217d876dbda02d1410848f575d
ojus <guptaojas75@gmail.com>
2025-06-23 13:23:57 +0530

    remove ref

--- drisllc-master/assets/scripts/logo.js
+++
@@ -1,8 +0,0 @@
-// const logo = document.querySelector(".logo");
-// logo.addEventListener("mouseenter", () => {
-//   document.body.classList.toggle("night");
-// });
-
-if (new URLSearchParams(window.location.search).has("night")) {
-  document.body.classList.add("night");
-}
--- drisllc-master/assets/scripts/work.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", () => {
-  const scrollY = Math.min(window.scrollY, 100);
-  const maxWidth = 42 + (scrollY / 400) * 20;
-  console.log(maxWidth);
-  document.querySelector("main").style.maxWidth = `${maxWidth}rem`;
-});
