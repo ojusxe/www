7b2e84822d9e41af304c7eae7e91484f92e2e6d6
ojus <guptaojas75@gmail.com>
2025-07-14 16:32:57 +0530

    add not found page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function NotFound() {
-  return <h2>Not Found</h2>;
+  return (
+    <div className="flex flex-col items-center justify-center max-h-screen gap-24 p-4">
+      <Image src="/xiao-fei-404.png" alt="Not Found" width={500} height={500} className="pointer-events-none"/>
+      <h4 className="text-sm font-mono text-muted-foreground">NOT FOUND</h4>
+    </div>
+  );
 }
