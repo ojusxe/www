dfc29742e1d7b0458dbe682c74bc435c9113053d
ojus <guptaojas75@gmail.com>
2025-11-11 11:28:30 +0530

    use nodejs runtime

--- client/src/app/manage/[[...tool]]/page.tsx
+++ client/src/app/manage/[[...tool]]/page.tsx
@@ -3,6 +3,9 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
+// Force this route to use Node.js runtime instead of Edge
+export const runtime = 'nodejs'
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
