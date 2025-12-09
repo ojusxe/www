71e9c2bbeb673e5bf78a0e93dfec4a4e08f715ae
ojus <guptaojas75@gmail.com>
2025-12-09 21:46:39 +0530

    cleanup + asset update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center max-h-screen gap-24 p-4">
-      <Image src="/xiao-fei-404.png" alt="Not Found" width={500} height={500} className="pointer-events-none"/>
+      <Image src="/ojusxe.webp" alt="Not Found" width={300} height={300} className="pointer-events-none"/>
       <h4 className="text-sm font-mono text-muted-foreground">NOT FOUND</h4>
     </div>
   );
